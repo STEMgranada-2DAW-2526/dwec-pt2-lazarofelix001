0d0d322b5d0ed12f9b9937e0e4910ee23d4fb7ce
Felixengithub <felixlazaro001.alu@stemgranada.com>
2025-11-25 10:55:41 +0100

    Importando imagenes

--- DefensorPoloNorte/src/App.jsx
+++ DefensorPoloNorte/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useReducer } from 'react'
 import './App.css'
+import './assets/arbol_laser.png';
+import './assets/canion_turron.png';
+import './assets/caramelo_sangriento.png';
+import './assets/multiplicador.png';
+import './assets/reno_lanza_cohetes.png';
+import './assets/torre.png';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const INITIAL_STATE = {
+  damageDealt: 0,
+  waveGoal: 100,
+  caramels:20,
+  damagePerShot:1,
+  autoShotPerSecond:1,
+  upgrades: [],
 }
-
-export default App
